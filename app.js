50ce8579e54db7522eebc947e96ca77b6944dffa
taesan1 <hts1304@naver.com>
2024-04-23 01:48:39 +0900

    1

--- app.js
+++ app.js
@@ -5,20 +5,20 @@ server.use(express.static(__dirname + "/public"));
 
 server.get("/", (req, res) => {
 
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/public/index.html");
 });
 
 server.get("/", (req, res) => {
 
-    res.sendFile(__dirname + "css/styles.css");
+    res.sendFile(__dirname + "/public/css/styles.css");
 });
 server.get("/", (req, res) => {
 
-    res.sendFile(__dirname + "js/scripts.js");
+    res.sendFile(__dirname + "/public/js/scripts.js");
 });
 server.get("/", (req, res) => {
 
-    res.sendFile(__dirname + "assets/styles.css");
+    res.sendFile(__dirname + "/public/assets/styles.css");
 });
 server.listen(3000, (err) => {
     if (err) return console.log(err);
