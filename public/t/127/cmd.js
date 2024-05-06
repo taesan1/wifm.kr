95773e1bfed106b2ee9a916f9cab7b3a8383ff1e
taesan1 <hts1304@naver.com>
2024-05-06 22:10:32 +0900

    1

--- public/t/127/cmd.js
+++ public/t/127/cmd.js
@@ -1,6 +1,6 @@
 javascript:
 //커맨드 메뉴 관리
-    var stop = localStorage.getItem('stop')  0;
+    var stop = localStorage.getItem('stop')|| 0;
 //모드 세팅 
 var mdd =localStorage.getItem('mdd')  0;;
 var md = prompt("1= 중지/실행 \n2= 모드 \n3= 스크립트 \n4= 초기화 \n5= 상태",mdd);
