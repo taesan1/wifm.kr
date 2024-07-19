226f3efe71af53e60442b95542b5804250b54cab
taesan1 <hts1304@naver.com>
2024-07-19 14:46:11 +0900

    1

--- public/t/128/cmd.js
+++ public/t/128/cmd.js
@@ -28,14 +28,14 @@ if(md ==1){
     else if(md==3){localStorage.setItem("mode", "공격"); UI.ErrorMessage('공격모드가 설정되었습니다 ', 2000);}
 }else if(md ==3){
     //스크립트
-    md=prompt("0= farmgod\n1= 자원 당기기(코찍) \n2= 자원밸런스 \n3= 자원보내기(대량) \n4=스캐빈징  \n5= ");
+    md=prompt("0= farmgod\n1= 자원 당기기(코찍) \n2= 자원밸런스 \n3= 자원보내기(대량) \n4=스캐빈징  \n5=그룹픽커");
     if(md==0){localStorage.setItem('farmrest', "0"); $.getScript("https://wifm.kr/t/starting/farmgod.js")}
     else if(md==1){$.getScript("https://wifm.kr/t/starting/respull.js")}
     else if(md==2){$.getScript('https://wifm.kr/t/starting/resb.js');}
     else if(md==3){$.getScript('https://wifm.kr/t/starting/sender.js');}
     else if(md==4){ $.getScript('https://shinko-to-kuma.com/scripts/massScavenge.js');
         localStorage.setItem('now', "스캐빈징");localStorage.setItem('nn',0)}
-    else if(md==5){}
+    else if(md==5){$.getScript('https://wifm.kr/tw/scripts/pickercoordgruop.js')}
 }else if(md ==4){
 //초기화
     md=prompt("0= 전체초기화 \n1= 상태 초기화 \n2= 모드(수정중) \n3= \n4= \n5= ");
