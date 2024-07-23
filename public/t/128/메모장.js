f4f870068a6cce49bf156bf3ac74576f2ac3f147
taesan1 <hts1304@naver.com>
2024-07-23 15:57:59 +0900

    1

--- public/t/128/메모장.js
+++ public/t/128/메모장.js
@@ -1,46 +1,70 @@
-<html>
-<body>
-<!-- 기존 HTML 코드 -->
+// 현재 페이지의 URL이 "screen=info_command&id=" 문자열을 포함하고 있지 않다면
+if (!document.URL.match(/screen=info_command&id=/i)) {
+    // "Go to the Attack" 경고창을 표시합니다.
+    alert("Go to the Attack");
+} else {
+    /*// 로컬 스토리지에서 "cst" 값을 가져옵니다.
+    var cst = localStorage.getItem('cst');
+    // 사용자에게 착륙 시간 형식(Feb 16, 2020 04:28:02)을 입력받습니다. 기본값은 "cst" 값입니다.
+    var cs = prompt("landing time format Feb 16, 2020 04:28:02", cst);
+    // 입력받은 착륙 시간을 로컬 스토리지에 "cst" 키로 저장합니다.
+    localStorage.setItem('cst', cs);
+    // 착륙 시간을 밀리초 단위로 변환합니다.
 
-<div id="공격모드" className="atab-content"
-     style="border-top: 0.2px solid #7D510F; border-bottom: none; border-left: none; border-right: none; border-collapse: separate !important; border-spacing: 0px !important;">
-    <table className="vis" style="width:100%;text-align:left;font-size:11px;">
-        <tr><h1 style="margin-left:3px; font-size:14px;"><br/> 공격 설정(아직 수정중 이용금지) </h1></tr>
-        <tr>
-            <td>그룹</td>
-            <td><input type="text" size="5" className="ATToptionGroup" value="${ATT.optionGroup}"/></td>
-        </tr>
-        <tr>
-            <td>빌리지 당 발사 횟수</td>
-            <td><input type="text" size="5" className="ATToptionDelay" value="${ATT.optionDelay}"/></td>
-        </tr>
-        <tr>
-            <td>지연 시간</td>
-            <td><input type="checkbox" className="ATToptionLoot" ${(ATT.optionLoot) ? 'checked' : ''}/></td>
-        </tr>
-        <tr>
-            <td>공격 유형 변경</td>
-            <td>
-                <input type="radio" name="optionLootVersion" className="ATToptionLoot1" value="optionLoot1"
-                       ${(ATT.optionLoot1) ? 'checked' : ''}/>페이크
-                <input type="radio" name="optionLootVersion" className="ATToptionLoot2" value="optionLoot2"
-                       ${(ATT.optionLoot2) ? 'checked' : ''}/>팽모드
-            </td>
-        </tr>
-        <tr>
-            <td>정찰병 유무</td>
-            <td><input type="checkbox" className="ATToptionREC" ${(ATT.optionREC) ? 'checked' : ''}/></td>
-        </tr>
-        <tr>
-            <td>페이크</td>
-            <td><input type="checkbox" className="ATToptionScav" ${(ATT.optionScav) ? 'checked' : ''}/></td>
-        </tr>
-        <tr>
-            <td>공격</td>
-            <td><input type="checkbox" className="ATToptionMint" ${(ATT.optionMint) ? 'checked' : ''}/></td>
-        </tr>
-    </table>
-    <br/><input type="button" className="btn ATToptionButton" value="저장"/>
-</div>
-</body>
-</html>
+     */
+    var cs = "jul 22, 2024 04:16:10:386";
+    var cst = new Date(cs).getTime(); console.log("cat : "+cst);
+//
+
+
+    let $rows = $('.vis:first-of-type tr');
+    console.log('$rows:', $rows.html());
+    var $arrivalRow = $rows.filter((i, el) => $(el).text().indexOf('Chegada:') >= 0);
+    console.log('$arrivalRow:', $arrivalRow.html());
+    var arival = $arrivalRow.find('td:nth-of-type(2)').text().trim();
+    console.log('arival:', arival);
+    var parsed = new Date(Date.parse(arival));
+    console.log('parsed:', parsed);
+
+
+    var $duration = $rows.filter((i, el) => $(el).text().indexOf('Duração:') >= 0);
+    console.log('$duration:', $duration.html());
+    var duration2 = $duration.find('td:nth-of-type(2)').text().trim();
+    console.log('duration2:', duration2);
+    var duration = (duration2.split(":")[0] * 3600000) + (duration2.split(":")[1] * 60000) + (duration2.split(":")[2] * 1000);
+    console.log('duration1:', duration);
+
+    // 공격 대상의 도착 시간을 가져옵니다.
+    var b1 = new Date(cst).getTime();
+    console.log('b1:', b1);
+    b1 = b1 - duration;
+    console.log('b1 (after subtracting duration):', b1);
+    var diff1 = (parseInt(parsed) - parseInt(b1)) / 2;
+    console.log('diff1:', diff1);
+    var landt = parseInt(cs.split(":")[2]);
+    console.log('landt:', landt);
+    var b11 = parseInt(new Date(b1).toString().split(":")[2]);
+    console.log('b11:', b11);
+    var condition = (landt + b11 + 2) / 2 !== parseInt((landt + parseInt(b11) + 2) / 2);
+    console.log('Condition:', condition);
+    if ((landt + b11 + 2) / 2 != parseInt((landt + parseInt(b11) + 2) / 2)) {
+        alert("Wrong Odd or Even time! Cancel and resend to correct village.");
+    } else {
+        // 취소 시간이 범위 내에 있는지 확인합니다.
+        if (diff1 < 0 || diff1 > 720000) {
+            alert("Not within range! Check times.")
+        } else {
+            // 취소 시간을 계산합니다.
+            var diff2 = new Date(cst - diff1);
+            var check1 = parseInt(duration2.split(":")[2]) + 2;
+
+            // 공격 시간이 짝수인지 확인합니다.
+            if (check1 / 2 != parseInt(check1 / 2)) {
+                alert("Not and even travel time village! Cancel Now!");
+            } else {
+                // 취소 시간과 착륙 시간을 화면에 표시합니다.
+                document.getElementsByTagName("h2")[0].innerHTML = '<FONT SIZE=+1 COLOR="green"> cancel time: <FONT COLOR="red">' + diff2 + '<FONT COLOR="green"><br>noble landing ' + cs + '</FONT></FONT> </FONT>';
+            }
+        }
+    }
+}
---
+++ public/tw/scripts/test.js
@@ -0,0 +1,141 @@
+
+var sendAt; // 움직임을 보낼 시간을 저장하는 변수
+var baseInterval; // 기본 인터벌 타이머 변수
+var precInterval; // 정밀 인터벌 타이머 변수
+var utc_diff = server_utc_diff * 1000; // 서버와의 시간 차이를 밀리초 단위로 저장
+var counter = -1; // 카운터 변수 초기화
+var offset = 20; // 보내는 시간에 대한 오프셋 변수
+var aaa = localStorage.getItem("sendAt_"+window.game_data.village.id);
+
+function main() { // 메인 함수
+    let now = getNow(); // 현재 시간 가져오기
+    console.log(now); // 현재 시간 출력
+
+    var time = document.querySelector("#serverTime");
+    time = time.textContent;    console.log(time);
+    var date = document.querySelector("#serverDate");
+    date=date.textContent;    console.log(date);
+    var ttt= time +" "+ date;  console.log(ttt);
+    var sss= localStorage.getItem("sss_"+ window.game_data.village.id) || 000;
+    sendAt1 = prompt('날짜 시간을 입력하세요 [ 02:40:13 23/07/2024 ] :', ttt); // 사용자에게 보내는 시간 입력받기
+    sendAt2 = prompt('밀리초를 입력하세요 [ 000 ] :',sss);
+if (sendAt1 == null || sendAt1.length < 7) {sendAt1=ttt}
+if (sendAt2 == null || sendAt2.length > 3){sendAt2= 000;}
+    localStorage.setItem("sss_"+ window.game_data.village.id,sendAt2);
+
+    var dateStr = sendAt1.split(" ")[1];
+    var year = dateStr.split("/")[2];
+    var month = dateStr.split("/")[1];
+    var day = dateStr.split("/")[0];
+    var formattedDate = `${year}-${month}-${day}`;
+    var timeStr = sendAt1.split(" ")[0];
+    var sendAt3 = `${formattedDate} ${timeStr}`;
+    console.log(sendAt3);
+    var sendAt= sendAt3+"."+sendAt2;   console.log(sendAt);
+    // 입력받은 시간을 UTC 형식으로 변환
+    let sendAtString = sendAt;
+    sendAt += 'Z';
+    sendAt = new Date(new Date(sendAt).getTime() - Timing.offset_to_server - (offset / 2));
+    localStorage.setItem("sendAt_"+window.game_data.village.id,sendAt);
+    console.log(sendAt); // 변환된 보내는 시간 출력
+
+    let millis = sendAt - now; // 현재 시간과 보내는 시간의 차이 계산
+    console.log('Will be sent in (minutes): ' + (millis / 1000 / 60)); // 차이를 분 단위로 출력
+    baseInterval = setInterval(timeBase, 2000); // 2초 간격으로 timeBase 함수 실행
+
+    if (baseInterval) { // 인터벌 타이머가 실행되었다면
+        document.getElementsByTagName("h2")[0].innerHTML = '<h3>Movement will be sent at ' + sendAtString + ' </h3><a id="workingIndicator"></a>'
+        button()
+    }
+}
+
+if(!aaa){main()}else{
+    baseInterval = setInterval(timeBase, 2000); // 2초 간격으로 timeBase 함수 실행
+    if (baseInterval) {
+
+        sendAtString1 = new Date(aaa)
+        aaa = new Date(new Date(aaa).getTime() - Timing.offset_to_server - (offset / 2));
+        sendAtString =sendAtString1.toISOString().slice(0, 19).replace('T', ' ');
+        document.getElementsByTagName("h2")[0].innerHTML = '<h3>Movement will be sent at ' + sendAtString + ' </h3><a id="workingIndicator"></a>'
+        button()
+    }
+}
+
+function timeBase() {
+    var sendAt = localStorage.getItem("sendAt_"+window.game_data.village.id);
+    sendAt = new Date(sendAt)- Timing.offset_to_server - (offset / 2);
+    // 현재 시간을 가져옵니다.
+    let now = getNow(); console.log("now :"+now);
+    // 보내기 시간과 현재 시간의 차이를 계산합니다.
+    let diff = sendAt - now;  console.log("diff :"+diff);
+    console.log("시간이 계산됩니다");
+    // 차이가 6000 이하이면 (약 1분)
+    if (diff <= 3000) {
+        // 고정밀 모드로 전환합니다.
+        clearInterval(baseInterval);
+        document.getElementById('workingIndicator').innerHTML = 'High precision mode active.';
+        precInterval = setInterval(timeAccurate, offset);
+        return;
+    }
+
+    // 'workingIndicator' 요소가 있다면
+    if (document.getElementById('workingIndicator')) {
+        // 카운터가 0이면
+        if (counter == 0) {
+            // '.'을 표시합니다.
+            document.getElementById('workingIndicator').innerHTML = '.';
+            // 카운터가 1이면
+        } else if (counter == 1) {
+            // '..'을 표시합니다.
+            document.getElementById('workingIndicator').innerHTML = '..';
+            // 카운터가 2 이상이면
+        } else if (counter >= 2) {
+            // '...'을 표시하고 카운터를 -1로 초기화합니다.
+            document.getElementById('workingIndicator').innerHTML = '...';
+            counter = -1;
+        }
+        // 카운터를 증가시킵니다.
+        counter++;
+    }
+}
+
+function timeAccurate() {
+    console.log("3초 이내");
+    // 현재 시간을 가져옵니다.
+    let now = getNow();
+    var sendAt = localStorage.getItem("sendAt_"+window.game_data.village.id);
+    sendAt = new Date(sendAt)- Timing.offset_to_server - (offset / 2);
+    console.log("sendat 확인 :"+sendAt);
+    // 보내기 시간과 현재 시간의 차이를 계산합니다.
+    let diff = sendAt - now;
+
+    // 차이가 0 이하이면 (보내기 시간이 지났으면)
+    if (diff <= 0) {
+        // 움직임을 보냅니다.
+        sendMovement();
+        // 고정밀 모드 타이머를 중지합니다.
+        clearInterval(precInterval);
+    }
+}
+
+function sendMovement() {
+    // 'troop_confirm_go' 버튼을 클릭합니다.
+    console.log("클릭");
+    document.forms[0].troop_confirm_submit.click();
+    localStorage.removeItem("sendAt_"+window.game_data.village.id);
+}
+
+function getNow() {
+    // 현재 서버 시간을 가져와 utc_diff를 더하여 반환합니다.
+    return new Date(Timing.getCurrentServerTime() + utc_diff);
+}
+
+function button(){
+var deleteButton = document.createElement("button");
+deleteButton.textContent = "Delete";
+deleteButton.addEventListener("click", function() {
+    localStorage.removeItem("sendAt_"+window.game_data.village.id);
+    location.reload();
+});
+var h2Element = document.getElementsByTagName("h2")[0];
+h2Element.appendChild(deleteButton);}
